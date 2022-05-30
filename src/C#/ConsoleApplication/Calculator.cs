751175a051f4e928bec58b95886af895177866f4
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2022-05-30 12:33:22 +0200

    Formel für jede größe sollte fertig sei

--- src/C#/ConsoleApplication/Calculator.cs
+++ src/C#/ConsoleApplication/Calculator.cs
@@ -2,44 +2,59 @@
 
 public class Calculator
 {
-    public List<Double[][]> GetResult(Double[][] matrixC)
+    public List<Double[,]> GetResult(Double[,] matrixC)
     {
-        Double[][] matrixA = new Double[matrixC.Length][];
-        Double[][] matrixB = new Double[matrixC.Length][];
+        Double[,] matrixA = new Double[matrixC.GetLength(1), matrixC.GetLength(1)];
+        Double[,] matrixB = new Double[matrixC.GetLength(1), matrixC.GetLength(1)];
         
         //fill zeros
-        for (int i = 0; i < matrixC.Length; i++)
+        for (int i = 0; i < matrixC.GetLength(1); i++)
         {
-            for (int j = 0; j < matrixC.Length; j++)
+            for (int j = 0; j < matrixC.GetLength(1); j++)
             {
-                matrixA[i][j] = 0;
-                matrixB[i][j] = 0;
+                matrixA[i,j] = 0;
+                matrixB[i,j] = 0;
             }
         }
 
         //fill ones
-        for (int i = 0; i < matrixC.Length; i++)
+        for (int i = 0; i < matrixC.GetLength(1); i++)
         {
-            matrixA[i][i] = 1;
+            matrixA[i,i] = 1;
         }
         
         //fill first B row
-        for (int i = 0; i < matrixC.Length; i++)
+        for (int i = 0; i < matrixC.GetLength(1); i++)
         {
-            matrixB[i][i] = matrixC[i][i];
+            matrixB[0,i] = matrixC[0,i];
         }
         
         //second row
-        for (int i = 1; i < matrixC.Length; i++)
+        for (int i = 1; i < matrixC.GetLength(1); i++)
         {
-            matrixA[i][0] = matrixC[i][0] / matrixB[0][0];
-            for (int j = 0; j < i; j++)
+            matrixA[i,0] = matrixC[i,0] / matrixB[0,0];
+            //fill A of row
+            for (int j = 1; j < i; j++)
             {
+                // 𝒂43 = (𝑐43 − 𝑎42 ⋅ 𝑏13)/𝑏33 
+                matrixA[i,j] = (matrixC[i,j] - matrixA[i,j-1] * matrixB[0,j] ) / matrixB[i-1,j];
+            }
+            
+            //fill B of row
+            for (int j = i; j < matrixC.GetLength(1); j++)
+            {
+                var number = 0.0;
                 
+                for (int k = 0; k < i; k++)
+                {
+                    number += matrixA[i,k] * matrixB[k,j];
+                }
+                
+                matrixB[i,j] = matrixC[i,j] - number;
             }
         }
         
-        var result = new List<Double[][]>();
+        var result = new List<Double[,]>();
         result.Add(matrixA);
         result.Add(matrixB);
         return result;
--- src/C#/ConsoleApplication/Program.cs
+++ src/C#/ConsoleApplication/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ConsoleApplication;
+using DefaultNamespace;
+
+Calculator calculator = new Calculator();
+
+var matrixC = new Double[3, 3] { {2.0, 7.0, 1.0}, {3.0, -2.0, 8.0}, {1.0, 5.0, 3.0} };
+var result = calculator.GetResult(matrixC);
+
+foreach (var matrix in result)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) {
+            Console.Write("{0} ", matrix[i, j]);
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    
+}
+
