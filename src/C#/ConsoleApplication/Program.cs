33839cbdb3c7fdd9aa701e023eddd8ed9809ea18
EliasAspoeck <elias.aspoeck@gmail.com>
2022-06-21 09:54:25 +0200

    add console output for greatly enhanced user experience

--- src/C#/ConsoleApplication/Program.cs
+++ src/C#/ConsoleApplication/Program.cs
@@ -5,8 +5,23 @@ using DefaultNamespace;
 
 Calculator calculator = new Calculator();
 
+/*
+  ____            _  _     _     __    ___            _____ 
+ |___ \          | || |   | |   /_ |  / _ \          | ____|
+   __) |   __ _  | || |_  | |_   | | | | | |  _ __   | |__  
+  |__ <   / _` | |__   _| | __|  | | | | | | | '_ \  |___ \ 
+  ___) | | (_| |    | |   | |_   | | | |_| | | | | |  ___) |
+ |____/   \__, |    |_|    \__|  |_|  \___/  |_| |_| |____/ 
+             | |                                            
+             |_|                                            
+*/
+
+Console.WriteLine("  ____            _  _     _     __    ___            _____ \n |___ \\          | || |   | |   /_ |  / _ \\          | ____|\n   __) |   __ _  | || |_  | |_   | | | | | |  _ __   | |__  \n  |__ <   / _` | |__   _| | __|  | | | | | | | '_ \\  |___ \\ \n  ___) | | (_| |    | |   | |_   | | | |_| | | | | |  ___) |\n |____/   \\__, |    |_|    \\__|  |_|  \\___/  |_| |_| |____/ \n             | |                                            \n             |_|");
+Console.WriteLine("The input is a matrix in the file format .csv\nPlease make sure to use a semicolon (;) to separate the numbers and to put a semicolon at the end of each line.");
+
 while (true)
 {
+    Console.Write("Enter the path of the .csv matrix file: ");
     var matrixC = CsvReader.readCSV(Console.ReadLine());
     var result = calculator.GetResult(matrixC);
 
@@ -20,7 +35,6 @@ while (true)
             Console.WriteLine();
         }
         Console.WriteLine();
-    
     }
 }
 
