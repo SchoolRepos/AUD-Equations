4461d88b68644000c0a89e8b0dc89bc6f5d97fbd
Tobias Wimmer <tobiwimma04@gmail.com>
2022-06-21 20:36:37 +0200

    add test template

--- src/Go/main_test.go
+++ src/Go/main_test.go
@@ -37,27 +37,38 @@ func TestCalculateAAndB3By3(t *testing.T) {
 }
 
 func TestCalculateAAndBXByX(t *testing.T) {
-	C := [][]float64{
-		{2, 7, 1},
-		{3, -2, 8},
-		{1, 5, 3},
+	type testCase struct {
+		C     [][]float64
+		WantA [][]float64
+		WantB [][]float64
 	}
-	wantA := [][]float64{
-		{1, 0, 0},
-		{1.5, 1, 0},
-		{0.5, -0.12, 1},
+	testCases := []testCase{
+		{
+			C: [][]float64{
+				{2, 7, 1},
+				{3, -2, 8},
+				{1, 5, 3},
+			},
+			WantA: [][]float64{
+				{1, 0, 0},
+				{1.5, 1, 0},
+				{0.5, -0.12, 1},
+			},
+			WantB: [][]float64{
+				{2, 7, 1},
+				{0, -12.5, 6.5},
+				{0, 0, 3.28},
+			},
+		},
 	}
-	wantB := [][]float64{
-		{2, 7, 1},
-		{0, -12.5, 6.5},
-		{0, 0, 3.28},
-	}
-	haveA, haveB := CalculateAAndBXByX(C)
-	if !floatSlicesEqual(haveA, wantA) {
-		t.Errorf("CalculateAAndB3By3(%v) returned A %v, want %v", C, haveA, wantA)
-	}
-	if !floatSlicesEqual(haveB, wantB) {
-		t.Errorf("CalculateAAndB3By3(%v) returned B %v, want %v", C, haveB, wantB)
+	for _, currentTestCase := range testCases {
+		haveA, haveB := CalculateAAndBXByX(currentTestCase.C)
+		if !floatSlicesEqual(haveA, currentTestCase.WantA) {
+			t.Errorf("CalculateAAndB3By3(%v) returned A %v, want %v", currentTestCase.C, haveA, currentTestCase.WantA)
+		}
+		if !floatSlicesEqual(haveB, currentTestCase.WantB) {
+			t.Errorf("CalculateAAndB3By3(%v) returned B %v, want %v", currentTestCase.C, haveB, currentTestCase.WantB)
+		}
 	}
 }
 
