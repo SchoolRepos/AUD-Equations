abeab15e66dfa3907b1ebc38cbcdd9117f40252e
Tobias Wimmer <tobiwimma04@gmail.com>
2022-06-21 20:14:36 +0200

    add go code, tests and binaries

--- src/Go/main.go
+++ src/Go/main.go
@@ -1,10 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fileName := "input.csv"
+	if len(os.Args) > 1 {
+		fileName = os.Args[1]
+	}
+	C, err := ReadCSV(fileName)
+	if err != nil {
+		log.Fatal("Error reading CSV:", err)
+	}
+
+	fmt.Println("3x3 Result:")
+	fmt.Println(CalculateAAndB3By3(C))
+
+	fmt.Println("X*X Result:")
+	fmt.Println(CalculateAAndBXByX(C))
 }
 
-func CalculateAAndB3By3(C [][]float64) (A, B [][]float64, err error) {
+func CalculateAAndB3By3(C [][]float64) (A, B [][]float64) {
 	A = [][]float64{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
 	B = [][]float64{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
 
@@ -17,8 +39,81 @@ func CalculateAAndB3By3(C [][]float64) (A, B [][]float64, err error) {
 	B[1][2] = C[1][2] - A[1][0]*B[0][2]
 
 	A[2][0] = C[2][0] / B[0][0]
-	A[2][1] = C[2][1] / B[0][1]
+	A[2][1] = (C[2][1] - A[2][0]*B[0][1]) / B[1][1]
 	B[2][2] = C[2][2] - A[2][0]*B[0][2] - A[2][1]*B[1][2]
 
 	return
+}
+
+func CalculateAAndBXByX(C [][]float64) (A, B [][]float64) {
+	A = make([][]float64, len(C))
+	B = make([][]float64, len(C))
+	for i := range A {
+		A[i] = make([]float64, len(C))
+		B[i] = make([]float64, len(C))
+	}
+
+	// Fill ones
+	for i := range C {
+		A[i][i] = 1
+	}
+
+	// Fill first B row
+	for i := range C {
+		B[0][i] = C[0][i]
+	}
+
+	// For each row
+	for i := range C {
+		A[i][0] = C[i][0] / B[0][0]
+
+		// Fill A of row
+		for j := 0; j < i; j++ {
+			var sum float64
+			for k := 0; k < j; k++ {
+				sum += A[i][k] * B[k][j]
+			}
+			A[i][j] = (C[i][j] - sum) / B[j][j]
+		}
+
+		// Fill B of row
+		for j := i; j < len(C[0]); j++ {
+			var number float64
+			for k := 0; k < i; k++ {
+				number += A[i][k] * B[k][j]
+			}
+			B[i][j] = C[i][j] - number
+		}
+	}
+
+	return
+}
+
+func ReadCSV(path string) ([][]float64, error) {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return [][]float64{}, err
+	}
+	lines := strings.Split(string(bytes), "\n")
+	matrix := [][]float64{}
+	for _, line := range lines {
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		row := []float64{}
+		parts := strings.Split(line, ";")
+		for _, part := range parts {
+			if strings.TrimSpace(part) == "" {
+				continue
+			}
+			num, err := strconv.ParseFloat(strings.ReplaceAll(part, ",", "."), 64)
+			if err != nil {
+				return matrix, err
+			}
+			row = append(row, num)
+		}
+		matrix = append(matrix, row)
+	}
+
+	return matrix, nil
 }
--- src/Go/main_test.go
+++ src/Go/main_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"reflect"
+	"math"
 	"testing"
 )
+
+const float64EqualityThreshold = 1e-9
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
+}
 
 func TestCalculateAAndB3By3(t *testing.T) {
 	C := [][]float64{
@@ -21,14 +27,53 @@ func TestCalculateAAndB3By3(t *testing.T) {
 		{0, -12.5, 6.5},
 		{0, 0, 3.28},
 	}
-	haveA, haveB, err := CalculateAAndB3By3(C)
-	if err != nil {
-		t.Errorf("CalculateAAndB3By3(%v) returned error: %v", C, err)
-	}
-	if !reflect.DeepEqual(haveA, wantA) {
+	haveA, haveB := CalculateAAndB3By3(C)
+	if !floatSlicesEqual(haveA, wantA) {
 		t.Errorf("CalculateAAndB3By3(%v) returned A %v, want %v", C, haveA, wantA)
 	}
-	if !reflect.DeepEqual(haveB, wantB) {
+	if !floatSlicesEqual(haveB, wantB) {
 		t.Errorf("CalculateAAndB3By3(%v) returned B %v, want %v", C, haveB, wantB)
 	}
+}
+
+func TestCalculateAAndBXByX(t *testing.T) {
+	C := [][]float64{
+		{2, 7, 1},
+		{3, -2, 8},
+		{1, 5, 3},
+	}
+	wantA := [][]float64{
+		{1, 0, 0},
+		{1.5, 1, 0},
+		{0.5, -0.12, 1},
+	}
+	wantB := [][]float64{
+		{2, 7, 1},
+		{0, -12.5, 6.5},
+		{0, 0, 3.28},
+	}
+	haveA, haveB := CalculateAAndBXByX(C)
+	if !floatSlicesEqual(haveA, wantA) {
+		t.Errorf("CalculateAAndB3By3(%v) returned A %v, want %v", C, haveA, wantA)
+	}
+	if !floatSlicesEqual(haveB, wantB) {
+		t.Errorf("CalculateAAndB3By3(%v) returned B %v, want %v", C, haveB, wantB)
+	}
+}
+
+func floatSlicesEqual(A, B [][]float64) bool {
+	if len(A) != len(B) {
+		return false
+	}
+	for rowIdx := range A {
+		if len(A[rowIdx]) != len(B[rowIdx]) {
+			return false
+		}
+		for colIdx := range A[rowIdx] {
+			if !almostEqual(A[rowIdx][colIdx], B[rowIdx][colIdx]) {
+				return false
+			}
+		}
+	}
+	return true
 }
